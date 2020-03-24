dca8136dccc3c38d7980d67b4e1c81ad025165bf
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-03-24 14:39:14 -0400

    finished character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+// Updates the character counter as 140 minus the characters in the text area
+// Turns red when counter is negative (i.e characters exceed 140)
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function(event) {
+    const textArea = $(this);
+    let textLength = textArea.val().length;
+    const counterDisplay = textArea.siblings('.counter');
+    counterDisplay.text(140 - textLength);
+    if (textLength <= 140) {
+      counterDisplay.removeClass('too-long');
+    } else {
+      counterDisplay.addClass('too-long');
+    }
+  });
+
+});
