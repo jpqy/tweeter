238f2ef007a9f6412b5df55f022ec4cd493c560a
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-03-25 14:57:32 -0400

    implemented loadTweets 😅

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -61,31 +61,14 @@ $(function() {
   });
 });
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-$(() => { renderTweets(data); });
+// Makes ajax request for tweets and renders them on page
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    type: 'GET'
+  })
+    .then(res => {
+      renderTweets(res);
+    });
+};
+loadTweets();
